bace646cda2bb4cfe8795ace1be9f1ce5e308cda
Demna Koridze <demnaqoridze@gmail.com>
2023-12-17 23:59:54 +0400

    changed

--- guess a number Game/main.cpp
+++ guess a number Game/main.cpp
@@ -1,14 +1,44 @@
-//
-//  main.cpp
-//  guess a number Game
-//
-//  Created by demnakoridze on 17.12.23.
-//
-
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int main(){
+    int userInput;
+
+    cout<<"You have to guess the number which is randomly generated, you have only 3 shoots";
+    srand(time(0));
+    int randomizer=rand()%100;
+    
+    
+    for(int cda=0; cda<3; cda++){
+        
+        cin >> userInput;
+        
+            if(userInput!=randomizer){
+                
+                if(userInput<randomizer){
+                    cout<<"Your input is smaller than randomly generated number"<<endl;
+                }else if(userInput>randomizer){
+                    cout<<"Your input is greater than randomly generated number"<<endl;
+                }
+                
+    
+            }else if(userInput==randomizer){
+                
+                cout << "Good job,you guessed the number";
+                break;
+            }
+        
+        
+        if(cda==2){
+            cout<<"Number was"<<randomizer<<endl;
+        }
+        
+    }
+        
+    
+   
     return 0;
 }
